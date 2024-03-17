336011e91bc6183f4f711da1268ca38bf1bdd18a
Mardin <mardinomer001@gmail.com>
2024-03-17 21:49:21 +0300

    algoritms

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWors='';
+  for (let i = word.length - 1; i >= 0; i --) {
+   reversedWors+= word[i];
+    
+  }
+  return word === reversedWors;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +18,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
 }
 
 module.exports = isPalindrome;
+
+
+
+
